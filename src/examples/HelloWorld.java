24ff941e7d4fb456b09c2775ca76c61aea894511
sAMpZ <amysampays@gmail.com>
2015-10-08 09:55:41 +0100

    Change 2

--- src/examples/HelloWorld.java
+++ src/examples/HelloWorld.java
@@ -3,7 +3,7 @@ package examples;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Cloudwick");
+		System.out.println("Hello Cloudwick, this is my second change!");
 
 	}
 
